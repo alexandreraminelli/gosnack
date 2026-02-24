a674ce9f91c5a9d4929af88b714f73413466e86b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-24 20:55:55 -0300

    chore(layout): organizar e documentar o `RootLayout`

--- gosnack-dashboard/src/app/layout.tsx
+++ gosnack-dashboard/src/app/layout.tsx
@@ -3,11 +3,19 @@ import "@/styles/globals.css"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
 
+/**
+ * Fonte Geist Sans do Google Fonts
+ * Usada para a maioria dos textos da aplicação.
+ */
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 })
 
+/**
+ * Fonte Geist Mono do Google Fonts.
+ * Usada para textos monoespaçados, como código ou tabelas.
+ */
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
@@ -34,8 +42,14 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang={SITE.locale}>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+    <html
+      lang={SITE.locale} // idioma da aplicação
+      suppressHydrationWarning // suprime avisos de hidratação do React
+      className={`${geistSans.variable} ${geistMono.variable}`}
+    >
+      <body className="antialiased">
+        {children} {/* Conteúdo aninhado */}
+      </body>
     </html>
   )
 }
