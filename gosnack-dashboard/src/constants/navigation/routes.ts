c2bfc4fb75300c61c9a6f8d3c26486acfc6364d2
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-25 15:07:47 -0300

    feat(routes): criar constante de rotas

---
+++ gosnack-dashboard/src/constants/navigation/routes.ts
@@ -0,0 +1,11 @@
+/**
+ * Caminhos das rotas da aplicação.
+ */
+export const ROUTES = {
+  // Autenticação
+  login: "/login",
+  forgotPassword: "/recuperar-senha",
+
+  // Dashboard
+  home: "/",
+} as const
