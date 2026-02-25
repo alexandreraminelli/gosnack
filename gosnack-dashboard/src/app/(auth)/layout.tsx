8def5f00abd0e69a71c5d27ef2f958586bfac58e
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-24 21:13:56 -0300

    feat(auth): criar layout das páginas de autenticação

---
+++ gosnack-dashboard/src/app/(auth)/layout.tsx
@@ -0,0 +1,38 @@
+import { Card, CardContent } from "@/components/ui/card"
+import { IMAGES } from "@/constants/images"
+import Image from "next/image"
+
+/**
+ * Layout das páginas de autenticação.
+ */
+export default function AuthLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="bg-muted min-h-svh flex flex-col items-center justify-center p-6 md:p-10 pt-0! gap-4">
+      {/* TODO: Header */}
+      <header className="flex flex-row items-center gap-8"></header>
+
+      {/* Card Central */}
+      <div className="w-full max-w-sm md:max-w-4xl">
+        <div className="flex flex-col gap-6">
+          <Card className="overflow-hidden p-0 min-h-96">
+            <CardContent className="grid p-0 md:grid-cols-2 h-full">
+              {/* Página */}
+              <div className="p-6 md:p-8">{children}</div>
+
+              {/* Imagem */}
+              <aside className="bg-muted relative max-md:hidden min-h-96">
+                <Image
+                  src={IMAGES.backgrounds.login}
+                  alt=""
+                  // Ajustar imagem
+                  fill
+                  className="object-cover"
+                />
+              </aside>
+            </CardContent>
+          </Card>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ gosnack-dashboard/src/app/(auth)/login/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Metadados da página de login.
+ */
+
+/**
+ * Página de login.
+ */
+export default function LoginPage() {
+  return <div>Login Page</div>
+}
