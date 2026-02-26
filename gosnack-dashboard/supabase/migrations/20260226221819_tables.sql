ef39f2c99c6c43311a7ea3f7c2ab3ea6411cf37d
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-26 19:22:54 -0300

    feat(database): definir estrutura da  tabela unidades

--- gosnack-dashboard/supabase/migrations/20260226221819_tables.sql
+++ gosnack-dashboard/supabase/migrations/20260226221819_tables.sql
@@ -20,3 +20,25 @@ create table users (
 create trigger trg_users_updated_at
 before update on users
 for each row execute function update_updated_at();
+
+-- Tabela de Unidades ----------------------------------------------------------
+
+create table units (
+    -- ID
+    id uuid primary key default gen_random_uuid(),
+    -- Nome da unidade
+    name text not null unique,
+    -- Data e hora de criação
+    created_at timestamp not null default now(),
+    -- Data e hora de atualização
+    updated_at timestamp not null default now(),
+    -- Se a unidade está ativa
+    is_active boolean not null default true
+);
+
+-- Trigger para atualizar updated_at automaticamente
+create trigger trg_units_updated_at
+before update on units
+for each row execute function update_updated_at();
+
+
