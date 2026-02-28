a4587337fc5755225f9f07ae519844c0f970d17b
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-27 21:35:13 -0300

    chore(layout): criar arquivo de layout das rotas privadas

---
+++ gosnack-dashboard/src/app/(private)/(overview)/page.tsx
@@ -0,0 +1,10 @@
+/**
+ * Página inicial do dashboard.
+ */
+export default function HomePage() {
+  return (
+    <div className="flex items-center justify-center w-full h-dvh">
+      <h1 className="text-2xl">GoSnack: Página Inicial</h1>
+    </div>
+  )
+}
---
+++ gosnack-dashboard/src/app/(private)/layout.tsx
@@ -0,0 +1,12 @@
+/**
+ * Layout das páginas privadas do dashboard.
+ */
+export default function PrivateLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div>
+      {/* TODO: Sidebar Provider */}
+      {/* TODO: Header */}
+      {children}
+    </div>
+  )
+}
