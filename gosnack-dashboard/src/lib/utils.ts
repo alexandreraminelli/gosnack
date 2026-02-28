b763fea9ad808f710daafc6d07d40e9cb06f2ad6
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-02-28 18:03:28 -0300

    feat(utils): criar utilitário para obter as  iniciais do usuário

--- gosnack-dashboard/src/lib/utils.ts
+++ gosnack-dashboard/src/lib/utils.ts
@@ -1,6 +1,26 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+/**
+ * Função utilitária para combinar classes CSS usando `clsx` e `tailwind-merge`.
+ *
+ * @param inputs - Lista de classes CSS ou objetos de classes.
+ *
+ * @returns String combinada de classes CSS, com conflitos resolvidos pelo
+ * `tailwind-merge`.
+ */
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
+}
+
+/**
+ * Função utilitária para obter as iniciais de um nome completo.
+ */
+export function getInitials(fullName: string) {
+  return fullName
+    .split(" ")
+    .map((name) => name[0])
+    .join("")
+    .toUpperCase()
+    .slice(0, 2)
 }
