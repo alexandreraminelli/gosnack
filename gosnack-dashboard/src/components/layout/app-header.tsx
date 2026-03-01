59f00f4aff7c1c67f0dc03781503c3d0b0f2326a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-03-01 19:02:59 -0300

    feat(header): alterar estilo e tamanho do ícone de abrir sidebar e tema

--- gosnack-dashboard/src/components/layout/app-header.tsx
+++ gosnack-dashboard/src/components/layout/app-header.tsx
@@ -10,15 +10,13 @@ export default function AppHeader() {
     <header className="bg-background/75 backdrop-blur sticky top-0 z-50 flex w-full items-center border-b">
       <div className="flex w-full items-center gap-2 px-4 h-16">
         {/* Botão de exibir/ocultar sidebar */}
-        <SidebarTrigger size="icon" variant="ghost" />
-
-        <Separator orientation="vertical" className="mx-2" />
+        <SidebarTrigger size="icon-lg" variant="outline" />
 
         {/* Breadcrumb */}
         <div className="w-full">{/* TODO: Header Breadcrumb */}</div>
 
         {/* Botão de tema */}
-        <ModeToggle />
+        <ModeToggle size="icon-lg" />
       </div>
     </header>
   )
