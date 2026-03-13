9cc651a244110ad926259fbc5304305a1f017968
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-03-13 20:40:36 -0300

    refactor(supabase-admin): remover `adminSupabase` desnecessário

--- gosnack-dashboard/src/lib/supabase/admin.ts
+++ gosnack-dashboard/src/lib/supabase/admin.ts
@@ -8,18 +8,12 @@ import { createClient } from "@supabase/supabase-js"
  *
  * @see https://supabase.com/docs/reference/javascript/admin-api
  */
-const adminClient = createClient(env.supabase.url, env.supabase.serviceRoleKey, {
+export const adminClient = createClient(env.supabase.url, env.supabase.serviceRoleKey, {
   auth: {
     autoRefreshToken: false, // Desativa o auto refresh de tokens, pois a chave de função de serviço não expira.
     persistSession: false, // Não persiste a sessão, já que a autenticação é feita via chave de função de serviço.
   },
 })
-
-/**
- * Cliente Supabase com permissão de service role.
- * Use apenas em Server Actions.
- */
-export const adminSupabase = adminClient
 
 /**
  * Cliente de autenticação para operações administrativas
