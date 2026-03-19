8b8aad3b3f0112ae4260eaf18de9996cd6ae7b1a
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-03-19 14:02:53 -0300

    refactor(password): criar zod schema separado para validação de senha

---
+++ gosnack-dashboard/src/features/authentication/password-recovery/schemas/password.schema.ts
@@ -0,0 +1,20 @@
+import { USERS_TEXTS } from "@/constants/texts/entities/users.texts"
+import { REGEX_PATTERNS } from "@/utils/validation/patterns"
+import { z } from "zod/v4"
+
+/**
+ * Zod Schema para criar ou redefinir a senha do usuário.
+ */
+export const passwordSchema = z.object({
+  /**
+   * Validação do campo de senha.
+   */
+  password: z
+    .string()
+    .nonempty(USERS_TEXTS.validation.password.required)
+    .min(8, USERS_TEXTS.validation.password.length) // comprimento
+    .regex(REGEX_PATTERNS.upper, USERS_TEXTS.validation.password.upperCase) // letra maiúscula
+    .regex(REGEX_PATTERNS.lower, USERS_TEXTS.validation.password.lowerCase) // letra minúscula
+    .regex(REGEX_PATTERNS.digit, USERS_TEXTS.validation.password.number) // número
+    .regex(REGEX_PATTERNS.specialChar, USERS_TEXTS.validation.password.specialChar), // caractere especial
+})
--- gosnack-dashboard/src/features/user-management/schemas/create-user.schema.ts
+++ gosnack-dashboard/src/features/user-management/schemas/create-user.schema.ts
@@ -1,6 +1,5 @@
 import { USERS_TEXTS } from "@/constants/texts/entities/users.texts"
 import { USER_ROLES } from "@/types/user.types"
-import { REGEX_PATTERNS } from "@/utils/validation/patterns"
 import { z } from "zod/v4"
 
 /**
@@ -19,17 +18,6 @@ export const createUserSchema = z.object({
    * Validação do campo de e-mail.
    */
   email: z.email(USERS_TEXTS.validation.email.invalid).nonempty(USERS_TEXTS.validation.email.required),
-  /**
-   * Validação do campo de senha.
-   */
-  password: z
-    .string()
-    .nonempty(USERS_TEXTS.validation.password.required)
-    .min(8, USERS_TEXTS.validation.password.length) // comprimento
-    .regex(REGEX_PATTERNS.upper, USERS_TEXTS.validation.password.upperCase) // letra maiúscula
-    .regex(REGEX_PATTERNS.lower, USERS_TEXTS.validation.password.lowerCase) // letra minúscula
-    .regex(REGEX_PATTERNS.digit, USERS_TEXTS.validation.password.number) // número
-    .regex(REGEX_PATTERNS.specialChar, USERS_TEXTS.validation.password.specialChar), // caractere especial
   /**
    * Validação do campo de papel do usuário.
    */
