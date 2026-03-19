b4779a04be494251b278b7053082b5990855c672
Alexandre Raminelli Junior <123258885+alexandreraminelli@users.noreply.github.com>
2026-03-19 12:55:07 -0300

    feat(user-management): criar service para enviar um convite por e-mail para outros usuários

--- gosnack-dashboard/src/features/user-management/services/user.service.ts
+++ gosnack-dashboard/src/features/user-management/services/user.service.ts
@@ -1,7 +1,9 @@
-import { UserProfile, UserRow } from "@/types/user.types"
+import { UserInsert, UserProfile, UserRow } from "@/types/user.types"
 import { adminClient } from "@/lib/supabase/admin"
 import { COLUMNS, TABLES } from "@/lib/supabase/schema"
 import { mapRowToUserProfile } from "@/utils/mappers/profile.mapper"
+import env from "@/lib/env"
+import { ROUTES } from "@/constants/navigation/routes"
 
 /**
  * Serviço para gerenciamento de usuários, incluindo listagem, criação,
@@ -19,5 +21,46 @@ export const userService = {
     if (error) throw error
 
     return data.map(mapRowToUserProfile)
+  },
+
+  /**
+   * Cria um novo usuário no sistema.
+   *
+   * @see https://supabase.com/docs/reference/javascript/auth-admin-inviteuserbyemail
+   */
+  async create(input: UserInsert): Promise<UserProfile> {
+    // Convidar usuário por e-mail pro Supabase Auth
+    const { data: authData, error: authError } = await adminClient.auth.admin.inviteUserByEmail(
+      input.email, // e-mail onde o convite será enviado
+      {
+        data: {
+          // Metadados do usuário
+          first_name: input.firstName,
+          last_name: input.lastName,
+          role: input.role,
+        },
+        // Link de redirecionamento após o usuário aceitar o convite e criar a senha
+        redirectTo: `${env.appUrl}/${ROUTES.auth.setPassword}`,
+      },
+    )
+    if (authError) throw authError // lançar erro
+
+    // Inserir perfil na tabela `users`
+    const { data: profileRow, error: dbError } = await adminClient
+      .from(TABLES.users)
+      .upsert({
+        id: authData.user.id, // UID do usuário no Supabase Auth
+        email: input.email,
+        first_name: input.firstName,
+        last_name: input.lastName,
+        role: input.role,
+        is_active: true,
+      } as UserRow)
+      .select()
+      .single()
+      .overrideTypes<UserRow, { merge: false }>()
+    if (dbError) throw dbError // lançar erro
+
+    return mapRowToUserProfile(profileRow as UserRow)
   },
 }
